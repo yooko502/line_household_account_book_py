b53c02c37b14adbaa73dcd987451cc3ab98d2771
yangxy <yang.xy0101@gmail.com>
2024-03-02 11:12:36 +0900

    update: port

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def handle_message(event):
                 )
 
 if __name__ == "__main__":
-      app.run(host="0.0.0.0", port=5000, debug=True)
+      app.run(host="0.0.0.0", port=10000, debug=True)
