163d0d29619f677dbdb9fdd5fbb3b97e596f428c
yangxy <yang.xy0101@gmail.com>
2024-03-02 17:14:41 +0900

    add: openai api

--- app.py
+++ app.py
@@ -2,13 +2,22 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
+from openai import OpenAI
 import os
 
 app = Flask(__name__)
 
 configuration = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 line_handler = WebhookHandler(os.environ['CHNNEL_SECRET_TOKEN'])
+OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
+client = OpenAI()
+client.api_key = OPENAI_API_KEY
 
+schema = {
+    "date": "YYYY-MM-DD",
+    "amount": "number",
+    "memo": "string"
+}
 @app.route('/', methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
@@ -24,8 +33,23 @@ def callback():
 
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-        message = TextSendMessage(text=event.message.text)
-        configuration.reply_message(event.reply_token, message)
+        user_message = TextSendMessage(text=event.message.text)
+        response = client.chat.completions.create(
+            model = 'gpt-3.5-turbo',
+            response_format={"type": "json_object"},
+            messages = [
+                {
+                    'role': 'system'
+                    , 'content': f"从以下字符串中提取date,amount,memo相关信息，并以json格式输出。JSON SCHEMA如下：{schema}"
+                }
+                , {
+                    'role': 'user'
+                    , 'content': user_message
+                }
+            ]
+        )
+
+        configuration.reply_message(event.reply_token, response.choices[0].message)
 
 if __name__ == "__main__":
       app.run(host="0.0.0.0", port=10000, debug=True)
