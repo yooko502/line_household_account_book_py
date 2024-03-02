a3a4cc8014824f0130a7bc5a5c9bd6243293c5e1
yangxy <yang.xy0101@gmail.com>
2024-03-02 11:01:56 +0900

    update:app.py

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, abort
+
+from linebot.v3 import (
+    WebhookHandler
+)
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
+)
+import os
+
+app = Flask(__name__)
+
+configuration = Configuration(os.environ['CHANNEL_ACCESS_TOKEN'])
+line_handler = WebhookHandler(os.environ['CHNNEL_SECRET_TOKEN'])
+
+@app.route('/callback', methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    body = request.get_data(as_text=True)
+    try:
+        line_handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@line_handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+        with ApiClient(configuration) as api_client:
+                line_bot_api = MessagingApi(api_client)
+                line_bot_api.reply_message_with_http_info(
+                    ReplyMessageRequest(
+                        reply_token=event.reply_token,
+                        messages=[TextMessage(text=event.message.text)]
+                    )
+                )
+
+if __name__ == "__main__":
+      app.run(host="0.0.0.0", port=5000, debug=True)
