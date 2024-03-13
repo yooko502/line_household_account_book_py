6644fa240b4de5310a11bc60f3995e9a7907b583
yangxy <yang.xy0101@gmail.com>
2024-03-13 20:16:40 +0900

    update google sheet api

--- app.py
+++ app.py
@@ -4,14 +4,15 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 import os
 from chatgpt_api import get_chatgpt_message
-from google_sheet_api import connect_gspread
+from google_sheet_api import connect_gspread, get_users_sheet_key, update_users_sheet_key
+import re
 
 app = Flask(__name__)
 
 configuration = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 line_handler = WebhookHandler(os.environ['CHNNEL_SECRET_TOKEN'])
 
-spread_sheet_key = os.environ['SPREAD_SHEET_KEY']
+# spread_sheet_key = os.environ['SPREAD_SHEET_KEY']
 
 @app.route('/', methods=['POST'])
 def callback():
@@ -28,13 +29,32 @@ def callback():
 
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+        user_id = None
+        if event.source.type == 'user':
+            user_id = event.source.userId
         user_message = event.message.text
+        if user_id is None:
+            configuration.reply_message(event.reply_token, TextSendMessage(text="请登录"))
+            return 
+        # 判断用户是否要登录自己的表格
+        pattern = r"google_sheet_key\[(.*?)\]"
+        match = re.match(pattern, user_message)
+        google_sheet_key = None
+        if match:
+            google_sheet_key = match.group(1)
+        
+        if google_sheet_key is not None:
+            update_users_sheet_key(user_id, google_sheet_key)
+            return_message = "你的账簿已经登录，现在可以更新你的账单了"
+            configuration.reply_message(event.reply_token, TextSendMessage(text=return_message))
+            return
+        
+        user_google_sheet_key_cell = get_users_sheet_key(user_id)
         # 调用chat gpt 进行分类
         json_message = get_chatgpt_message(user_message)
-        jsonf = "/etc/secrets/GOOGLE_SECRETS_JSON"
         # 把信息登录到google sheet
         # TODO: 1. line menu 接受用户的表格id，根据用户来更新用户的表格（spread_sheet_key 需要替换为用户的）
-        connect_gspread(jsonf, spread_sheet_key, json_message)
+        connect_gspread(user_google_sheet_key_cell, json_message)
         # line bot 返回信息
         return_message = \
         f"""=== 账单已登录 ===\n日期: {json_message['date']}\n消费内容: {json_message['memo']}\n金额: {json_message['amount']}"""
--- google_sheet_api.py
+++ google_sheet_api.py
@@ -1,13 +1,16 @@
+import os
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-
 
 SHEET_CELL = {
     "DATA": 1,
     "MEMO": 2,
     "AMOUNT": 3
 }
-def connect_gspread(jsonf, key, json_message):
+jsonf = "/etc/secrets/GOOGLE_SECRETS_JSON"
+
+# 更新每个人的表单
+def connect_gspread(key, json_message):
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
     credentials = ServiceAccountCredentials.from_json_keyfile_name(jsonf, scope)
     gc = gspread.authorize(credentials)
@@ -16,6 +19,49 @@ def connect_gspread(jsonf, key, json_message):
     # ws = connect_gspread(jsonf, spread_sheet_key)
     data = ws.get_all_values()
     row_number = len(data)
-    ws.update_cell(row_number + 1, SHEET_CELL['DATA'] , json_message['date'])
-    ws.update_cell(row_number + 1, SHEET_CELL['MEMO'], json_message['memo'])
-    ws.update_cell(row_number + 1, SHEET_CELL['AMOUNT'], json_message['amount'])
+    if row_number == 0:
+        ws.update_cell(1, 1 , '日期')
+        ws.update_cell(1, 2 , '花销内容')
+        ws.update_cell(1, 3 , '金额')
+        ws.format(row=1, col=1, backgroundColor="#3CB371")
+        ws.format(row=1, col=2, backgroundColor="#3CB371")
+        ws.format(row=1, col=3, backgroundColor="#3CB371")
+    else:
+        ws.update_cell(row_number + 1, SHEET_CELL['DATA'] , json_message['date'])
+        ws.update_cell(row_number + 1, SHEET_CELL['MEMO'], json_message['memo'])
+        ws.update_cell(row_number + 1, SHEET_CELL['AMOUNT'], json_message['amount'])
+
+# 将用户的key登录
+def update_users_sheet_key(user_id, google_sheet_key):
+    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(jsonf, scope)
+    gc = gspread.authorize(credentials)
+    # 记录数据用的表单key
+    SPREADSHEET_KEY = os.environ['SPREAD_SHEET_KEY']
+    ws = gc.open_by_key(SPREADSHEET_KEY).sheet1
+    # ws = connect_gspread(jsonf, spread_sheet_key)
+    data = ws.get_all_values()
+    row_number = len(data)
+    user_cell = ws.find(user_id)
+    if row_number == 0:
+        ws.update_cell(1, 1 , 'user_id')
+        ws.update_cell(1, 2 , 'user_key')
+    elif user_cell is not None:
+        # 同一个用户更新为新的表单
+        ws.update_cell(user_cell.row, user_cell.col + 1, google_sheet_key)
+    else:
+        ws.update_cell(row_number + 1, 1 , user_id)
+        ws.update_cell(row_number + 1, 2 , google_sheet_key)
+
+# 获取用户key
+def get_users_sheet_key(user_id):
+    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(jsonf, scope)
+    gc = gspread.authorize(credentials)
+    SPREADSHEET_KEY = os.environ['SPREAD_SHEET_KEY']
+    ws = gc.open_by_key(SPREADSHEET_KEY).sheet1
+    # ws = connect_gspread(jsonf, spread_sheet_key)
+    user_cell = ws.find(user_id)
+    google_sheet_key_cell = ws.cell(user_cell.row, user_cell.col + 1).value
+
+    return google_sheet_key_cell
