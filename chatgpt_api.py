1dd2968bf7586032fb2acb938a2fd4bad7e2db81
yangxy <yang.xy0101@gmail.com>
2024-03-26 20:43:10 +0900

    update date format

--- chatgpt_api.py
+++ chatgpt_api.py
@@ -19,7 +19,7 @@ def get_chatgpt_message(user_message):
             messages = [
                 {
                     'role': 'system',
-                    'content': f"从以下字符串中提取date,amount,memo相关信息,如果不含date信息,则date补充为空值,JSON SCHEMA如下:{schema}"
+                    'content': f"从以下字符串中提取date,amount,memo相关信息,如果日期不符合年份的情况(例:0201,03/01,3/1),请用YYYY-MM-DD填充,并以json格式输出。JSON SCHEMA如下:{schema}"
                 }
                 , {
                     'role': 'user'
@@ -28,7 +28,7 @@ def get_chatgpt_message(user_message):
             ]
         )
     json_message = json.loads(response.choices[0].message.content)
-    if not json_message.get('date'):
+    if json_message['date'] == "YYYY-MM-DD":
         json_message['date'] = datetime.date.today().strftime("%Y/%m/%d")
     if json_message['date'].startswith("YYYY"):
         current_year = datetime.datetime.now().year
