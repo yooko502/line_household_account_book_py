f204846477e5883a79c07e6a9b2dc10734ca1b3f
yangxy <yang.xy0101@gmail.com>
2024-03-28 22:29:16 +0900

    update total function

--- line_rich_menu_api.py
+++ line_rich_menu_api.py
@@ -11,55 +11,29 @@ line_handler = WebhookHandler(os.environ['CHNNEL_SECRET_TOKEN'])
 
 def create_rich_menu():
     rich_menu_to_create = RichMenu(
-        size = RichMenuSize(height=585,width=1040),
+        size = RichMenuSize(height=843,width=2500),
         selected=False,
         name="Rich menu",
         chat_bar_text="Tap here",
         areas=[
                     RichMenuArea(
-                        bounds=RichMenuBounds(x=0, y=0, width=520, height=585),
+                        bounds=RichMenuBounds(x=0, y=0, width=834, height=843),
                         action=MessageAction(label='Person Form', text='個人登録')
                     ),
                     RichMenuArea(
-                        bounds=RichMenuBounds(x=521, y=0, width=520, height=585),
+                        bounds=RichMenuBounds(x=835, y=0, width=834, height=843),
                         action=MessageAction(label='Group Form', text='グループ登録')
+                    ),
+                    RichMenuArea(
+                        bounds=RichMenuBounds(x=1669, y=0, width=832, height=843),
+                        action=MessageAction(label='Total', text='合計')
                     )
                 ]
     )
 
-    total_rich_menu_to_create = RichMenu(
-        size = RichMenuSize(height=585,width=1040),
-        selected=False,
-        name="Total Rich menu",
-        chat_bar_text="合計",
-        areas=[
-                    RichMenuArea(
-                        bounds=RichMenuBounds(x=0, y=0, width=1040, height=585),
-                        action=MessageAction(label='Person Form', text='合計')
-                    ),
-                ]
-    )
-
     rich_menu_id = configuration.create_rich_menu(rich_menu=rich_menu_to_create)
-    total_rich_menu_id = configuration.create_rich_menu(rich_menu=total_rich_menu_to_create)
 
     with open("static/richmessage.png", 'rb') as f:
         configuration.set_rich_menu_image(rich_menu_id, 'image/png', f)
-    
-    with open("static/richmenu_total.png", 'rb') as f:
-        configuration.set_rich_menu_image(total_rich_menu_id, 'image/png', f)
 
     configuration.set_default_rich_menu(rich_menu_id)
-    configuration.set_default_rich_menu(total_rich_menu_id)
-
-    alias_a = CreateRichMenuAliasRequest(
-            rich_menu_alias_id='richmenu-alias-a',
-            rich_menu_id=rich_menu_id
-        )
-    configuration.create_rich_menu_alias(alias_a)
-
-    alias_b = CreateRichMenuAliasRequest(
-            rich_menu_alias_id='richmenu-alias-b',
-            rich_menu_id=total_rich_menu_id
-        )
-    configuration.create_rich_menu_alias(alias_b)
